bdfa97b65cd9485d56c195b0fa97b8d734479a12
phai <pekopon_@live.fr>
2024-01-20 11:28:07 +0700

    add footer, add nav bar, add not found page, add login page, add register page

---
+++ components/footer.tsx
@@ -0,0 +1,76 @@
+import Image from "next/image";
+import SwensenIcon from "./swensen-icon";
+
+function Footer() {
+  return (
+    <section className="fixed bottom-0 w-full">
+      <footer className="flex p-6 bg-[#e21c23] items-center flex-col lg:flex-row gap-5">
+        <SwensenIcon />
+        <div className="lg:ms-auto">
+          <ul className="flex font-semibold xl:text-xl text-white flex-wrap justify-center">
+            <li className="mx-3 hover:cursor-pointer">
+              Brandsite
+            </li>
+            <li className="mx-3 hover:cursor-pointer">
+              Privilege
+            </li>
+            <li className="mx-3 hover:cursor-pointer">
+              Reward
+            </li>
+            <li className="mx-3 hover:cursor-pointer">
+              My Coupons
+            </li>
+            <li className="mx-3 hover:cursor-pointer">
+              Gift Voucher
+            </li>
+            <li className="mx-3 hover:cursor-pointer">
+              Member Card
+            </li>
+            <li className="mx-3 hover:cursor-pointer">
+              My Account
+            </li>
+          </ul>
+        </div>
+      </footer>
+      <footer className="flex px-6 py-4 bg-[#cb191f] items-center flex-col-reverse sm:flex-row gap-5">
+        <div>
+          <ul className="flex">
+            <li className="mx-2 hover:cursor-pointer">
+              <Image
+                src="/icon-facebook.svg"
+                alt="facebook Logo"
+                width={24}
+                height={25}
+              />
+            </li>
+            <li className="mx-2 hover:cursor-pointer">
+              <Image
+                src="/icon-instagram.svg"
+                alt="instagram Logo"
+                width={24}
+                height={25}
+              />
+            </li>
+            <li className="mx-2 hover:cursor-pointer">
+              <Image
+                src="/icon-youtube.svg"
+                alt="youtube Logo"
+                width={24}
+                height={25}
+              />
+            </li>
+          </ul>
+        </div>
+        <div className="sm:ms-auto">
+          <ul className="flex text-white flex-wrap justify-center">
+            <li className="mx-3 hover:cursor-pointer">FAQ</li>
+            <li className="mx-3 hover:cursor-pointer">Terms and Conditions</li>
+            <li className="mx-3 hover:cursor-pointer">Privacy Policy</li>
+          </ul>
+        </div>
+      </footer>
+    </section>
+  )
+}
+
+export default Footer
---
+++ components/navbar.tsx
@@ -0,0 +1,73 @@
+import Link from "next/link"
+import SwensenIconHead from "./swensen-icon-head"
+
+function NavBar() {
+  return (
+    <section className="lg:h-[72px] h-[102px] bg-white shadow-md flex fixed ps-[24px] w-full top-0 flex-col">
+      <div className="flex justify-between lg:justify-normal w-full grow">
+        <div className="lg:hidden flex items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 hover:cursor-pointer">
+            <path fillRule="evenodd" d="M3.75 2A1.75 1.75 0 0 0 2 3.75v3.5C2 8.216 2.784 9 3.75 9h3.5A1.75 1.75 0 0 0 9 7.25v-3.5A1.75 1.75 0 0 0 7.25 2h-3.5ZM3.5 3.75a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.5a.25.25 0 0 1-.25.25h-3.5a.25.25 0 0 1-.25-.25v-3.5ZM3.75 11A1.75 1.75 0 0 0 2 12.75v3.5c0 .966.784 1.75 1.75 1.75h3.5A1.75 1.75 0 0 0 9 16.25v-3.5A1.75 1.75 0 0 0 7.25 11h-3.5Zm-.25 1.75a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.5a.25.25 0 0 1-.25.25h-3.5a.25.25 0 0 1-.25-.25v-3.5Zm7.5-9c0-.966.784-1.75 1.75-1.75h3.5c.966 0 1.75.784 1.75 1.75v3.5A1.75 1.75 0 0 1 16.25 9h-3.5A1.75 1.75 0 0 1 11 7.25v-3.5Zm1.75-.25a.25.25 0 0 0-.25.25v3.5c0 .138.112.25.25.25h3.5a.25.25 0 0 0 .25-.25v-3.5a.25.25 0 0 0-.25-.25h-3.5Zm-7.26 1a1 1 0 0 0-1 1v.01a1 1 0 0 0 1 1h.01a1 1 0 0 0 1-1V5.5a1 1 0 0 0-1-1h-.01Zm9 0a1 1 0 0 0-1 1v.01a1 1 0 0 0 1 1h.01a1 1 0 0 0 1-1V5.5a1 1 0 0 0-1-1h-.01Zm-9 9a1 1 0 0 0-1 1v.01a1 1 0 0 0 1 1h.01a1 1 0 0 0 1-1v-.01a1 1 0 0 0-1-1h-.01Zm9 0a1 1 0 0 0-1 1v.01a1 1 0 0 0 1 1h.01a1 1 0 0 0 1-1v-.01a1 1 0 0 0-1-1h-.01Zm-3.5-1.5a1 1 0 0 1 1-1H12a1 1 0 0 1 1 1v.01a1 1 0 0 1-1 1h-.01a1 1 0 0 1-1-1V12Zm6-1a1 1 0 0 0-1 1v.01a1 1 0 0 0 1 1H17a1 1 0 0 0 1-1V12a1 1 0 0 0-1-1h-.01Zm-1 6a1 1 0 0 1 1-1H17a1 1 0 0 1 1 1v.01a1 1 0 0 1-1 1h-.01a1 1 0 0 1-1-1V17Zm-4-1a1 1 0 0 0-1 1v.01a1 1 0 0 0 1 1H12a1 1 0 0 0 1-1V17a1 1 0 0 0-1-1h-.01Z" clipRule="evenodd" />
+          </svg>
+        </div>
+        <Link href={'/'} >
+          <SwensenIconHead />
+        </Link>
+        <div className="ms-auto lg:flex hidden">
+          <div className="flex justify-center items-center text-[#787878] border-x-[1px] w-[328px] px-7 font-semibold me-6 hover:text-[#e21c23] hover:cursor-pointer">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 me-2">
+              <path fillRule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clipRule="evenodd" />
+            </svg>
+            <p>Select delivery address</p>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 ms-[26px]">
+              <path fillRule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+            </svg>
+          </div>
+          <div className="leading-[70px]">
+            <Link className="me-6 min-w-[140px] h-[48px] inline-block text-center rounded-[50px] leading-[46px] font-light border-[1px] border-[#e21c23] text-[#e21c23]"
+            href={'/register'} >
+              Register
+            </Link>
+          </div>
+          <div className="leading-[70px]">
+            <Link className="me-6 min-w-[140px] h-[48px] inline-block text-center rounded-[50px] leading-[46px] font-light border-[1px] bg-[#e21c23] text-white hover:bg-[#e21c23]/80"
+            href={'/login'} >
+              Login
+            </Link>
+          </div>
+          <div className="flex items-center gap-2 text-[#787878] border-s-[1px] px-6 hover:text-[#e21c23] hover:cursor-pointer font-semibold">
+            EN
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+              <path fillRule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+            </svg>
+          </div>
+        </div>
+        <div className="lg:hidden flex items-center me-6 gap-4">
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 hover:cursor-pointer">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+            </svg>
+          </div>
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 hover:cursor-pointer">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className="lg:hidden leading-9">
+        <div className="flex items-center text-[#787878] w-fit font-semibold me-6 hover:text-[#e21c23] hover:cursor-pointer">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 me-2">
+            <path fillRule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clipRule="evenodd" />
+          </svg>
+          <p>Select delivery address</p>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 ms-[26px]">
+            <path fillRule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+          </svg>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default NavBar
