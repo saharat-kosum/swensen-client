b92a78137544feef69e9c962cc74a0d2fb70d9e2
phai MacOS <pekopon_@live.fr>
2024-03-12 11:36:30 +0700

    edit middleware to check api also

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,42 @@
 // middleware.ts
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { isLogin } from "./utils/auth";
+import { isLogin, verifyToken } from "./utils/auth";
 
 export async function middleware(request: NextRequest) {
-  const login = await isLogin();
-  if (login) {
-    return NextResponse.next();
-  } else {
-    return NextResponse.redirect(new URL("/login", request.url));
+  const { pathname } = request.nextUrl;
+
+  if (pathname.startsWith("/admin")) {
+    const isLoggedIn = await isLogin();
+    if (isLoggedIn) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
   }
+
+  if (pathname.startsWith("/api")) {
+    const isPublicEndpoint =
+      pathname.includes("/login") || pathname.includes("/register");
+
+    if (isPublicEndpoint) {
+      return NextResponse.next();
+    }
+
+    const token = await verifyToken();
+    if (token) {
+      return NextResponse.next();
+    } else {
+      return Response.json(
+        { message: "authentication failed" },
+        { status: 401 }
+      );
+    }
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/admin/:path*"],
+  matcher: ["/admin/:path*", "/api/:path*"],
 };
